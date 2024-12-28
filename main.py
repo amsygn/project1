b4688d6df0dc2fe8f209f562c600bf04dd18b5e8
Anton Mosyagin <mosyagin@yandex.ru>
2024-12-28 20:00:20 +0500

    updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from src.masks import get_mask_card_number, get_mask_account, card_num, account_num
+from src.widget import get_mask_card_number
 
-print('Маска номера карты:')
-print(get_mask_card_number(card_num))
-print('Маска номера счета:')
-print(get_mask_account(account_num))
+print(get_mask_card_number())
+
