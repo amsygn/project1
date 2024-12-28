dd7e60706328c5473ca3113ca76f10e69186e3fc
Anton Mosyagin <mosyagin@yandex.ru>
2024-12-28 20:02:17 +0500

    added src/widget.py. hw completed

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,23 @@
-from masks import get_mask_card_number, get_mask_account
+from .masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_input: str) -> str:
-    """ Возвращает строку с замаскированным номером карты или счета"""
+    """Возвращает строку с замаскированным номером карты или счета"""
 
     input_split = user_input.split()
     # определяем формат номера - карта или счет
     if len(input_split[-1]) == 16:
         return get_mask_card_number(input_split[-1])
     else:
-        return get_mask_account(input_split[-1])
+        return get_mask_account(int(input_split[-1]))
 
 
 def get_date(user_date: str) -> str:
-    """ Возвращает строку с датой в формате ДД.ММ.ГГГГ """
-    pass
+    """Возвращает строку с датой в формате ДД.ММ.ГГГГ"""
+    return user_date[8:10] + "." + user_date[5:7] + "." + user_date[0:4]
 
-user_input: str = input('>')
-print(mask_account_card(user_input))
+
+# user_input: str = input('>')
+# print(mask_account_card(user_input))
+
+# print(get_date('2024-03-11T02:26:18.671407'))
