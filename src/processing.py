47b24562f1c29eb1f13cbc3686c2f19d1f1c501e
Anton Mosyagin <mosyagin@yandex.ru>
2025-01-06 23:06:21 +0500

    main.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,5 @@
 from typing import List
 
-data_logs = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-]
-
 def filter_by_state(data_logs: List[dict], state: str='EXECUTED') -> List[dict]:
     """ Возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению. """
@@ -20,9 +13,7 @@ def filter_by_state(data_logs: List[dict], state: str='EXECUTED') -> List[dict]:
     return data_filter
 
 
-def sort_by_date(date_logs: dict):
+def sort_by_date(data_logs: List[dict], reverse='True') -> List[dict]:
     """ Возвращает новый список, отсортированный по дате """
-    pass
 
-
-print(filter_by_state(data_logs), sep='\n')
+    return sorted(data_logs, key=lambda x: x['date'], reverse=reverse)
--- src/widget.py
+++ src/widget.py
@@ -16,9 +16,3 @@ def mask_account_card(user_input: str) -> str:
 def get_date(user_date: str) -> str:
     """Возвращает строку с датой в формате ДД.ММ.ГГГГ"""
     return user_date[8:10] + '.' + user_date[5:7] + '.' + user_date[0:4]
-
-
-# user_input: str = input('>')
-# print(mask_account_card(user_input))
-
-# print(get_date('2024-03-11T02:26:18.671407'))
