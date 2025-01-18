41a7a6b2a7ffc890e9958ce7d1aed8bfcb69d80e
Anton Mosyagin <mosyagin@yandex.ru>
2025-01-18 18:50:56 +0500

    mod README.md

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,8 @@ from typing import List
 
 
 def filter_by_state(data_logs: List[dict], state: str = "EXECUTED") -> List[dict]:
-    """Возвращает новый список словарей, содержащий только те словари,
-    у которых ключ state соответствует указанному значению."""
+    """ Возвращает новый список словарей, содержащий только те словари,
+    у которых ключ state соответствует указанному значению. """
 
     data_filter = []
 
@@ -14,7 +14,7 @@ def filter_by_state(data_logs: List[dict], state: str = "EXECUTED") -> List[dict
     return data_filter
 
 
-def sort_by_date(data_logs: List[dict], reverse="True") -> List[dict]:
-    """Возвращает новый список, отсортированный по дате"""
+def sort_by_date(data_logs: List[dict], reverse: bool=True) -> List[dict]:
+    """ Возвращает новый список, отсортированный по дате """
 
     return sorted(data_logs, key=lambda x: x["date"], reverse=reverse)
