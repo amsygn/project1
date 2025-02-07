390a006fe78485d4e7b684b07715a768ddb5c68d
Anton Mosyagin <mosyagin@yandex.ru>
2025-02-07 23:34:52 +0500

    added tests/test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -52,3 +52,13 @@ def test_card_number_generator():
     assert next(card_num_gen) == "0000 0000 0001 0002"
 
 
+@pytest.mark.parametrize("input,expected_output", [
+    ("0000 0000 0000 0001", ["0000 0000 0000 0001"]),
+    ("0000 0000 0000 0002", ["0000 0000 0000 0002"]),
+    ("0000 0000 0000 0003", ["0000 0000 0000 0003"]),
+    ("0000 0000 0000 0004", ["0000 0000 0000 0004"])
+])
+def test_number_generator(input, expected_output):
+    generator = card_number_generator(input, 4)
+    for expected in expected_output:
+        assert next(generator) == expected
