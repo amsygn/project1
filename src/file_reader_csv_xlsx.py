f2b716e5c56bf669818a7fcbb6bdfd0ee5fee909
Anton Mosyagin <mosyagin@yandex.ru>
2025-03-01 17:44:37 +0500

    mod src/file_reader_csv_xlsx.py + requirements.txt

--- src/file_reader_csv_xlsx.py
+++ src/file_reader_csv_xlsx.py
@@ -1,1 +1,22 @@
-# read_csv_transactions` и `read_excel_transactions
+import csv
+import os
+import pandas as pd
+from config import DATA_DIR
+
+def read_csv(data: str) -> None:
+    file_path = os.path.join(DATA_DIR, data)
+    with open (file_path, encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            print(row)
+
+def read_excel(data: str) -> None:
+    file_path = os.path.join(DATA_DIR, data)
+    excel_data = pd.read_excel(file_path)
+    print(f"Размерность файла: {excel_data.shape}")
+    print(excel_data.head(3))
+
+
+
+# print(read_csv("transactions.csv"))
+# print(read_excel("transactions_excel.xlsx"))
